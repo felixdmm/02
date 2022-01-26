33821716b1b2392afa3584e893f61aae5b3299ef
Felixdmm <felixdmm@gmail.com>
2022-01-26 19:57:20 +0100

    Archivo variables.py añadaido y sefundo coment en apuntes.py

---
+++ Comandos.py
@@ -0,0 +1,7 @@
+# cd "ruta" - change directory
+# git status - 
+# git init - 
+# git add "nombre del archivo"; hacer seguimiento y pasar archivo a zona de preparacion
+# git commit -m "mensaje que quieras que salga"
+# git log --oneline
+
