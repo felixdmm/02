462dd0d30db730695c5ab42b9a3217c3e8a11cc2
Felixdmm <felixdmm@gmail.com>
2022-01-26 19:59:39 +0100

    Variable añadida y tercer comentario

--- Comandos.py
+++ Comandos.py
@@ -4,4 +4,4 @@
 # git add "nombre del archivo"; hacer seguimiento y pasar archivo a zona de preparacion
 # git commit -m "mensaje que quieras que salga"
 # git log --oneline
-
+# git add . - añade todos los archivos del directorio a la preparación
